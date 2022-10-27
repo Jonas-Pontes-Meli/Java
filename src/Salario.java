dea55e4fc9d91dfacb3e893bdb8eea8be8f898fa
Jonas-Pontes-Meli <jonas.pontes@mercadolivre.com>
2022-10-27 14:05:05 -0300

    Aula_1 Atividade 1

---
+++ src/Salario.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Salario {
+    public static void main(String[] args) {
+        double salario;
+        double novoSalario;
+        double abono;
+        Scanner leia = new Scanner(System.in);
+        System.out.println("Digite seu Salário : ");
+        salario = leia.nextDouble();
+        System.out.println("Digite o Valor do Abono : ");
+        abono = leia.nextDouble();
+        novoSalario = salario + abono;
+        System.out.println("O seu novo Salário é : "+ novoSalario);
+
+
+    }
+}
